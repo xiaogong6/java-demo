1477f6dca973e22d2d3904dcbd3cb1ee4dd259d6
xiongke <917634854@qq.com>
2024-03-29 10:12:44 +0800

    feat: :sparkles: 项目初始化

---
+++ demo-collection/src/main/java/com/java/democollection/DemoCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.java.democollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCollectionApplication.class, args);
+    }
+
+}
---
+++ demo-collection/src/test/java/com/java/democollection/DemoCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.democollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
