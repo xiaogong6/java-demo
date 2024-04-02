df5160fee238a13eda73527eddf5c22eebfc8be9
xiongke <917634854@qq.com>
2024-04-02 13:19:16 +0800

    feat: :sparkles: demo初始化

---
+++ demo-collection/src/main/java/com/java/democollection/arrayList/HashMapTest.java
@@ -0,0 +1,30 @@
+package com.java.democollection.arrayList;
+
+import com.google.common.collect.Maps;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @Program: demo-java
+ * @Description:
+ * @Author: xiongke
+ * @Create: 2024-04-01
+ */
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = Maps.newHashMapWithExpectedSize(7);
+
+        List<String> list = Arrays.asList("1", "2", "3");
+
+        Stream<String> stream = list.stream();
+
+        Stream<String> stringStream = list.parallelStream();
+
+        // stringStream.reduce()
+    }
+
+}
