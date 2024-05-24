885f05654e7e2d8fa79a7b764903f191f8caf5f6
xiongke <917634854@qq.com>
2024-05-24 16:57:26 +0800

    feat: :sparkles: JVM炼丹

---
+++ demo-jvm/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Program: Default (Template) Project
+ * @Description: ${description}
+ * @Author: xiongke
+ * @Create: 2024-05-24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
